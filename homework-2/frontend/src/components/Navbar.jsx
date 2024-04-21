7cc568e2ab889b04b1b653855b94832a3bf1c481
Yohan Prakoso <prakoso.yohan@gmail.com>
2024-04-22 01:07:14 +0800

    put some logic in Navbar

--- homework-2/frontend/src/components/Navbar.jsx
+++ homework-2/frontend/src/components/Navbar.jsx
@@ -1,19 +1,40 @@
 import { Box, Button, Flex, HStack, Heading, Spacer, Text, Avatar } from "@chakra-ui/react";
 import TabNav from "./TabNav";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
-	const { isLogedIn, setIsLogedIn } = useState(false)
+	const [ isLogedIn, setIsLogedIn ]= useState(false)
+	const navigate = useNavigate()
+
+	useEffect(()=>{
+		if(localStorage.getItem("token")) setIsLogedIn(true);
+		else setIsLogedIn(false);
+	}, [])
+
+	function onLoginClicked(){
+		navigate("/login");
+	}
+
+	function onLogoutClicked(){
+		window.alert("Logout Cuy");
+	}
 
 	return (
 		<Flex as="nav" p="30px" alignItems="center" bg="yellow">
 			<Heading as="h1" pr="20px">My Books Site</Heading>
-			<TabNav />
+			{isLogedIn && <TabNav />}
+			
 			<Spacer />
 			<HStack spacing="20px">
-				<Avatar bg="yellow.500" />
-				<Text>yohan@gmail.com</Text>
-				<Button colorScheme="yellow">Logout</Button>
+				{
+					isLogedIn && 
+					<>
+						<Avatar bg="yellow.500" />
+						<Text>yohan@gmail.com</Text>
+					</>
+				}
+				<Button colorScheme="yellow" onClick={isLogedIn?onLogoutClicked:onLoginClicked}>{isLogedIn? "Logout" : "Login"}</Button>
 			</HStack>
 		</Flex>
 	)
